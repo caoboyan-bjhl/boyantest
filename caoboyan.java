80c868867d2ca3c911fe941516d364e097eb186a
caoboyan <caoboyan@baijiahulian.com>
2014-09-01 16:25:30 +0800

    tt

---
+++ caoboyan.java
@@ -0,0 +1,39 @@
+
+public class Person {
+private String name;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getSex() {
+	return sex;
+}
+public void setSex(String sex) {
+	this.sex = sex;
+}
+public String getHeight() {
+	return height;
+}
+public void setHeight(String height) {
+	this.height = height;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getTel() {
+	return tel;
+}
+public void setTel(String tel) {
+	this.tel = tel;
+}
+private String sex;
+private String height;
+private String address;
+private String tel;
+
+}
